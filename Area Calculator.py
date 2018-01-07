659dd8fa70dc84baf708e25a228c1380f7f9d622
mbarbour0 <m.barbour0217@me.com>
2018-01-07 16:29:47 -0500

    Retaking Python courses

--- Area Calculator.py
+++ Area Calculator.py
@@ -1,4 +1,4 @@
-# This program will help you in maths
+# Area Calculator
 
 from math import pi
 from time import sleep
@@ -6,29 +6,29 @@ from datetime import datetime
 
 now = datetime.now()
 
-print "The calculator is now starting, prepare to di... ahem, prepare to have me help you human master"
+print 'Your area calculator is now starting up...'
 
-print "%s/%s/%s %s:%s" % (now.month, now.day, now.year, now.hour, now.minute)
+print 'The current time is %02d/%02d/%04d %02d:%02d' % (now.month, now.day, now.year, now.hour, now.minute)
 
 sleep(1)
 
-hint = "Don't forget to include the correct units! \nExiting.."
+hint = 'Don\'t forget to include the correct units! \nExiting...'
 
-option = raw_input("Enter C for Circle or T for Triangle: ")
+option = raw_input('Enter C for Circle or T for Triangle: ')
 option = option.upper()
 
-if option == "C":
-  radius = float(raw_input("Enter radius: "))
+if option == 'C':
+  radius = float(raw_input('Input a Radius for your Circle: '))
   area = pi * (radius ** 2)
-  print "The pie is baking..."
+  print 'The pie is baking...'
   sleep(1)
-  print "%.2f" % (area) + "\n" + hint
-elif option == "T":
-  base = float(raw_input("Please enter the base "))
-  height = float(raw_input("Please enter the height "))
-  area = 0.5 * base * height
-  print "Uni Bi Tri..."
+  print 'The Area of your Circle is %.02f.' % (area) + ' ' + hint
+elif option == 'T':
+  base = float(raw_input('Enter a Base for your Triangle: '))
+  height = float(raw_input('Enter a Height for your Triangle: '))
+  area = .5 * base * height
+  print 'Uni Bi Tri...'
   sleep(1)
-  print "%.2f" % (area) + "\n" + hint
+  print 'The area of your Triangle is %.02f.' % (area) + ' ' + hint
 else:
-  print "You've entered garbage, son. I'm closing this project down..."
+  print 'You\'ve entered hot garbagio, please try again.'
--- Dice Guess.py
+++ Dice Guess.py
@@ -1,41 +1,44 @@
-#dice game
-
 from random import randint
 from time import sleep
 
 def get_user_guess():
-  guess = int(input('What\'s your guess? '))
-  return guess
+  user_guess = int(raw_input('Enter your guess: '))
+  return user_guess
 
 def roll_dice():
-  sides = int(input('How many sides do your dice have? '))
-  if sides >0:
-    first_roll = randint(1, sides)
-    second_roll = randint(1, sides)
-    max_val = sides * 2
-    print ("This is the highest possible roll " + str(max_val))
+  number_of_sides = int(raw_input('Enter the number of sides your dice has: '))
+  if number_of_sides > 1:
+    first_roll = randint(1, number_of_sides)
+    second_roll = randint(1, number_of_sides)
+    total_roll = first_roll + second_roll
+    max_val = number_of_sides * 2
+    print 'The maximum roll you can achieve is %s' % (str(max_val))
     sleep(1)
     user_guess = get_user_guess()
-    if (user_guess > max_val) or user_guess == 1:
-      print ('Your guess of ' + str(user_guess) + ' is invalid')
-    else:
-      print ('Rolling...')
+    if user_guess <= max_val and user_guess >= 2:
+      print 'Rolling...'
       sleep(2)
-      print ('The first roll is ' + str(first_roll))
+      print 'The computer\'s first roll is %d.' % (first_roll)
       sleep(1)
-      print ('The second roll is ' + str(second_roll))
+      print 'The computer\'s second roll is %d.' % (second_roll)
       sleep(1)
-      total_roll = first_roll + second_roll
-      print (total_roll)
-      print ('Result...')
+      print 'The computer\'s total roll is %d.' % (total_roll)
       sleep(1)
       if user_guess > total_roll:
-        print ('You did it, you won!!')
+        print 'Looks like you win this time.'
+        return
       elif user_guess < total_roll:
-        print ('You lose, better luck next time..')
+        print 'You lose this one biosack.'
+        return
       elif user_guess == total_roll:
-        print ('You tied, I demand a rematch!')
+        print 'Your human brain has guessed my number, somehow.'
+        return
+      
+    else:
+      print 'Enter a valid guess'
+      return
   else:
-    print('You have attempted to cheat me!')
+    print 'Enter a valid number of sides for your dice.'
+    return
 
 roll_dice()
