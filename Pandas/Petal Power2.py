8ca921074908980acad9824b056fc98b860a8f3b
mbarbour0 <matthew.barbour@icloud.com>
2018-02-03 14:34:09 -0500

    More Pandas Practice

---
+++ Pandas/Petal Power2.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+inventory = pd.read_csv('inventory.csv')
+
+staten_island = inventory[inventory.location == 'Staten Island']
+product_request = staten_island.product_description.unique()
+seed_request = inventory[(inventory.location == 'Brooklyn') & (inventory.product_type == 'seeds')]
+inventory['in_stock'] = inventory.quantity.apply(lambda x: True if x > 0 else False)
+inventory['total_value'] = inventory.price * inventory.quantity
+
+"""
+print(product_request)
+
+for i in product_request:
+  print(i)
+
+print(seed_request)
+"""
+
+print(inventory)
