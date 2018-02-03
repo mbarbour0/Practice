e49f9ce6094a070065a4fb4327b6f1c4d5afa615
mbarbour0 <matthew.barbour@icloud.com>
2018-02-03 16:57:42 -0500

    Pivots and Aggregate Functions

--- Pandas/Pandas3.py
+++ Pandas/Pandas3.py
@@ -1,13 +1,24 @@
 import pandas as pd
+import numpy as np
 
 orders = pd.read_csv('orders.csv')
 
 most_expensive = orders.price.max()
 num_colors = orders.shoe_color.nunique()
+pricey_shoes = orders.groupby('shoe_type').price.max().reset_index()
+expensive_shoes = orders.groupby('shoe_type').price.apply(lambda x: np.percentile(x, 75))
+cheap_shoes = orders.groupby('shoe_color').price.apply(lambda x: np.percentile(x, 25)).reset_index()
+shoe_counts = orders.groupby(['shoe_type', 'shoe_color']).id.count().reset_index()
+shoe_counts = shoe_counts.rename(columns={'id': 'counts'})
 
-# """
-print(most_expensive)
-print(num_colors)
-# """
+
+# print(most_expensive)
+# print(num_colors)
+# print(pricey_shoes)
+# print(type(pricey_shoes))
+# print(cheap_shoes)
+# print(expensive_shoes)
+print(shoe_counts)
+
 
 # print(orders.head(10))
