e349a9d911c48e2a63646e44c9d0c449fd363276
mbarbour0 <matthew.barbour@icloud.com>
2018-02-03 19:34:22 -0500

    Pivots and Aggregates Lesson

--- Pandas/Pandas3.py
+++ Pandas/Pandas3.py
@@ -10,7 +10,7 @@ expensive_shoes = orders.groupby('shoe_type').price.apply(lambda x: np.percentil
 cheap_shoes = orders.groupby('shoe_color').price.apply(lambda x: np.percentile(x, 25)).reset_index()
 shoe_counts = orders.groupby(['shoe_type', 'shoe_color']).id.count().reset_index()
 shoe_counts = shoe_counts.rename(columns={'id': 'counts'})
-
+shoe_counts_pivot = shoe_counts.pivot(columns = 'shoe_color', index = 'shoe_type', values = 'counts').reset_index()
 
 # print(most_expensive)
 # print(num_colors)
@@ -18,7 +18,8 @@ shoe_counts = shoe_counts.rename(columns={'id': 'counts'})
 # print(type(pricey_shoes))
 # print(cheap_shoes)
 # print(expensive_shoes)
-print(shoe_counts)
+# print(shoe_counts)
+print(shoe_counts_pivot)
 
 
 # print(orders.head(10))
---
+++ Pandas/Pandas4.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+user_visits = pd.read_csv('page_visits.csv')
+click_source = user_visits.groupby('utm_source').id.count().reset_index()
+click_source_by_month = user_visits.groupby(['utm_source', 'month']).id.count().reset_index()
+click_source_by_month_pivot = click_source_by_month.pivot(columns = 'month', index = 'utm_source', values = 'id').reset_index()
+
+print(click_source_by_month_pivot)
