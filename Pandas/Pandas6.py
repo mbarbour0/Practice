6e790f6df7846de2458b81caa7a0e81127ae5d2e
mbarbour0 <matthew.barbour@icloud.com>
2018-02-04 21:53:05 -0500

    Merging Tables

---
+++ Pandas/Pandas6.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+sales = pd.read_csv('sales.csv')
+print(sales)
+targets = pd.read_csv('targets.csv')
+print(targets)
+
+sales_vs_targets = pd.merge(sales, targets)
+
+print(sales_vs_targets)
