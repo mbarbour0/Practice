ea1a30a2ec3d4814a9a26c1f6d3ee9d1275cdab7
mbarbour0 <matthew.barbour@icloud.com>
2018-02-05 22:36:11 -0500

    Merging

--- Pandas/Pandas6.py
+++ Pandas/Pandas6.py
@@ -1,14 +1,17 @@
 import pandas as pd
 
 sales = pd.read_csv('sales.csv')
-print(sales)
 targets = pd.read_csv('targets.csv')
-print(targets)
-
 sales_vs_targets = pd.merge(sales, targets)
-
-print(sales_vs_targets)
-
 crushing_it = sales_vs_targets[sales_vs_targets.revenue > sales_vs_targets.target]
+men_women = pd.read_csv('men_women_sales.csv')
+all_data = sales.merge(targets).merge(men_women)
+results = all_data[(all_data.revenue > all_data.target) & (all_data.women > all_data.men)]
 
-print(crushing_it)
+print(sales.head(3))
+print(targets.head(3))
+print(sales_vs_targets.head(5))
+print(crushing_it.head(5))
+print(men_women.head(3))
+print(all_data.head(3))
+print(results.head(5))
