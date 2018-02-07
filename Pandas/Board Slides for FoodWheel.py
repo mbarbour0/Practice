5ba828661428a48caf0aa2be62525a67978d0aa3
mbarbour0 <matthew.barbour@icloud.com>
2018-02-06 22:09:53 -0500

    Plotting Pie Chart

--- Pandas/Board Slides for FoodWheel.py
+++ Pandas/Board Slides for FoodWheel.py
@@ -4,7 +4,14 @@ import pandas as pd
 restaurants = pd.read_csv('restaurants.csv')
 cuisine_options_count = restaurants.cuisine.nunique()
 cuisine_counts = restaurants.groupby('cuisine').name.count().reset_index()
+cuisines = cuisine_counts.cuisine
+counts = cuisine_counts.name
+
 
 print(restaurants)
 print(cuisine_options_count)
 print(cuisine_counts)
+
+plt.pie(counts, labels = cuisines, autopct='%d%%')
+plt.axis = 1
+plt.show()
