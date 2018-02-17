46e7dc7e7890b0456d3e12bde4af1edd81e8c63e
mbarbour0 <matthew.barbour@icloud.com>
2018-02-17 17:37:52 -0500

    Cleaning up

--- Conversion v Efficiency/Conversion Efficiency.py
+++ Conversion v Efficiency/Conversion Efficiency.py
@@ -12,7 +12,10 @@ hours_by_agent.to_csv('hours_by_agent.csv', index=False)
 print(hours_by_agent.head())
 """
 
-hours_by_agent = pd.read_csv('hours_by_agent.csv')
+# hours_by_agent = pd.read_csv('hours_by_agent.csv')
+
+hour_work = pd.read_excel('URC 10-15 to 12-7.xlsx')
+print(hour_work.head())
 
 """
 apt_1015 = pd.read_excel('October Appointments 10-15 to 10-20.xlsx')
@@ -30,33 +33,3 @@ count_by_agent.to_csv('count_by_agent.csv', index=False)
 count_by_agent = pd.read_excel('count_by_agent.xlsx')
 print(count_by_agent)
 """
-
-"""
-print(clt_cog.head())
-print(ls_cog.head())
-print(orl_cog.head())
-print(twd_cog.head())
-"""
-
-urc = openpyxl.load_workbook('URC 10-15 to 12-7.xlsx')
-hours_logged = urc.get_sheet_by_name('Sheet1')
-
-def get_sec(time_str):
-    """Returns time in seconds"""
-    try:
-        h, m, s = time_str.split(':')
-        return int(h) * 3600 + int(m) * 60 + int(s)
-    except ValueError:
-        pass
-
-dict1 = {}
-
-for i in range(1, 500):
-    if hours_logged.cell(row = i + 1, column = 4).value != None:
-        e = str(hours_logged.cell(row = i + 9, column = 25).value)
-        eout = get_sec(e)
-        try:
-            result = round(((aout + bout + cout + dout) / (eout)) * 100, 2)
-        except TypeError:
-            continue
-        dict1[name] = result
